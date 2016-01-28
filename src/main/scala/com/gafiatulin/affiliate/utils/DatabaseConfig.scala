9cbd3feefca6842c41a26ac3d203e639ce75d8e2
Victor Gafiatulin <gafiatulin@gmail.com>
2016-01-28 18:57:47 +0300

    fix stupid bug

--- src/main/scala/com/gafiatulin/affiliate/utils/DatabaseConfig.scala
+++ src/main/scala/com/gafiatulin/affiliate/utils/DatabaseConfig.scala
@@ -3,5 +3,5 @@ package com.gafiatulin.affiliate.utils
 trait DatabaseConfig {
     val driver = slick.driver.PostgresDriver
     import driver.api._
-    def db = Database.forConfig("database")
+    val db = Database.forConfig("database")
 }

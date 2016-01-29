86d7594081bf2879babba40cd8f43bc1ccd65145
Victor Gafiatulin <gafiatulin@gmail.com>
2016-01-29 03:17:47 +0300

    change duid

--- src/main/scala/com/gafiatulin/affiliate/Service.scala
+++ src/main/scala/com/gafiatulin/affiliate/Service.scala
@@ -4,7 +4,7 @@ import java.util.UUID
 import scala.concurrent.Future
 import scala.concurrent.ExecutionContext.Implicits.global
 
-import com.gafiatulin.affiliate.utils.Config 
+import com.gafiatulin.affiliate.utils.Config
 import com.gafiatulin.affiliate.models.{RefCodeEntity, RefCodeEntityTable}
 
 object Service extends Service
@@ -15,7 +15,7 @@ trait Service extends RefCodeEntityTable with Config {
 
     def getCodeById(id: String):Future[Option[RefCodeEntity]] = db.run(codes.filter(_.id === id).result.headOption)
     def update(id: String, f: RefCodeEntity => RefCodeEntity):Future[Option[RefCodeEntity]] = getCodeById(id).flatMap{
-        case Some(code) => 
+        case Some(code) =>
             val updatedCode = f(code)
             db.run(codes.filter(_.id === id).update(updatedCode)).map(_ => Some(updatedCode))
         case None => Future.successful(None)
@@ -26,18 +26,19 @@ trait Service extends RefCodeEntityTable with Config {
     }
 
     def signUp(ref: String) = {
-        val duid = UUID.nameUUIDFromBytes((databaseUrl + UUID.randomUUID().toString).getBytes()).toString.replaceAll("-", "")
+        val duid = new UUID(UUID.nameUUIDFromBytes(databaseUrl.getBytes).getLeastSignificantBits,
+                            UUID.randomUUID.getLeastSignificantBits).toString.replaceAll("-", "")
         val id = insert(RefCodeEntity(duid)).map{_.id}
         id.flatMap(x => upPR(ref))
         id
     }
 
     def upV(id: String) = update(id, {x => x.copy(visits = x.visits + 1)})
-    
+
     def upR(id: String) = update(id, {x => x.copy(regs = x.regs + 1)})
 
     def upPR(id: String) = update(id, {x => x.copy(partnerRegs = x.partnerRegs + 1)})
-    
+
     def statsFor(id: String) = getCodeById(id).map{
         case Some(ce) => "Visits: " + ce.visits +  " Registrations: " + ce.regs + " Partner Regsistrations: " + ce.partnerRegs
         case None => "No stats found for id: " + id
